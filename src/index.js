730386fd5ac17c8dffeac134afd1e94410da5686
diogoshiroma <diogoshiroma@hotmail.com>
2020-02-19 12:01:26 -0300

    Add hot reload and .gitignore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { TravelSimpleApp } from './app';
+import 'react-hot-loader/patch';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(
-	<TravelSimpleApp />,
-	document.getElementById('root'),
-);
+
+const render = Component => {
+	ReactDOM.render(
+	  <AppContainer>
+		<Component />
+	  </AppContainer>,
+	  document.getElementById('root')
+	);
+  }
+
+  render(TravelSimpleApp);
+
+  if (module.hot) {
+	module.hot.accept('./app', () => { render(TravelSimpleApp) });
+  }
