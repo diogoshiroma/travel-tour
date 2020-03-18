f378ee010ff3ab7c4acb107a18a7b2d3601564cf
diogoshiroma <diogoshiroma@hotmail.com>
2020-03-18 10:31:08 -0300

    Fix calendar dates

--- src/app/data/mapper/mapper.tsx
+++ src/app/data/mapper/mapper.tsx
@@ -1,13 +1,27 @@
 import { CalendarEvent } from "../model/calendar-event";
 
 export const mapToCalendarEvent = (data: any) => {
+  const startYear = data.start_date.substring(0, 4)
+  let startMonth = data.start_date.substring(5, 7)
+  const startDay = data.start_date.substring(8, 10)
+
+  const endYear = data.end_date.substring(0, 4)
+  let endMonth = data.end_date.substring(5, 7)
+  const endDay = data.end_date.substring(8, 10)
+
+  startMonth = startMonth - 1
+  endMonth = endMonth - 1
+  
+  var endDate = new Date(endYear, endMonth, endDay)
+  endDate.setDate(endDate.getDate()+1)
+
   return {
     id: data.pk,
     host_id: data.host_id,
     title: data.host_name,
     bedroom_id: data.bedroom_id,
     bedroom_name: data.bedroom_name,
-    start: new Date(data.start_date),
-    end: new Date(data.end_date)
+    start: new Date(startYear, startMonth, startDay),
+    end: endDate
   };
 };
